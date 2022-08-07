a0c98971e0d41f534688743862e327e874fa5a0c
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2022-08-07 15:28:12 -0700

    added question #1 / logic.js v2

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,13 +1,22 @@
 // logic.js code starts here
-var timeLeft = 90;
+
 var timerID;
+var timeLeft = 90;
 var timerEl = document.getElementById("timer");
 var startButton = document.getElementById("start-btn");
 var nextButton = document.getElementById("next-btn");
 var questionContainerEl = document.getElementById("question-container");
 var startContainerEl = document.getElementById("start-container");
 var questionEl = document.getElementById("question");
-
+var answerButtonsEl = document.getElementById("answer-buttons");
+var checkAnswerEl = document.getElementById("check-answer");
+var viewHighScores = document.getElementById("highscores-link");
+var submitButton = document.getElementById("submit-btn");
+var clearScoreButton = document.getElementById("clear-btn");
+var initialsField = document.getElementById("player-name");
+var restartButton = document.getElementById("restart-btn");
+var scoreField = document.getElementById("player-score");
+var scores = JSON.parse(localStorage.getItem("scores")) || [];
 
 
 
@@ -40,4 +49,27 @@ function startGame() {
     // Timer will start as soon as start button is clicked
     timeTick();
     setNextQuestion();
+};
+
+
+// Go to next question
+function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
+
+
+// Display questions
+function showQuestion(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsEl.appendChild(button)
+    })
 };
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,7 +1,13 @@
  // questions.js code here
 
 var questions = [
-    {
-        
-    }
+    { 
+        question: "How do you write 'Hello World' in an alert box?", 
+        answers: [
+            { text: "msg('Hello World')", correct: false },
+            { text: "alert('Hello World')" , correct: true },
+            { text: "prompt('Hello World')", correct: false },
+            { text: "alertBox('Hello World')", correct: false }
+        ]
+    },
 ]
