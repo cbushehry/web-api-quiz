58d8c593a40ff831c627773be04c9c8e665ae1a3
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2022-08-07 14:32:47 -0700

    added coutdown timer and start quiz button js v1

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,43 @@
-// logic.js code here
+// logic.js code starts here
+var timeLeft = 90;
+var timerID;
+var timerEl = document.getElementById("timer");
+var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
+var questionContainerEl = document.getElementById("question-container");
+var startContainerEl = document.getElementById("start-container");
+var questionEl = document.getElementById("question");
+
+
+
+
+// Start button trigger the first question and next button to display
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+});
+
+
+// Countdown timer
+function timeTick() {
+    timeLeft--;
+    timerEl.textContent = "Time: " + timeLeft;
+    if (timeLeft <= 0) {
+        saveScore();
+    }
+}
+
+
+// Start Quiz
+function startGame() {
+    timerID = setInterval(timeTick, 1000);
+    startContainerEl.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerEl.classList.remove("hide");
+
+    // Timer will start as soon as start button is clicked
+    timeTick();
+    setNextQuestion();
+};
